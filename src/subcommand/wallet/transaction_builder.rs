77acf3c4f5a7292d783a34ed0c019a9992b6bf75
renfakai <rfk1118@gmail.com>
2023-09-18 15:13:12 +0800

    change 546

--- src/subcommand/wallet/transaction_builder.rs
+++ src/subcommand/wallet/transaction_builder.rs
@@ -118,8 +118,8 @@ impl TransactionBuilder {
   const ADDITIONAL_INPUT_VBYTES: usize = 58;
   const ADDITIONAL_OUTPUT_VBYTES: usize = 43;
   const SCHNORR_SIGNATURE_SIZE: usize = 64;
-  pub(crate) const TARGET_POSTAGE: Amount = Amount::from_sat(10_000);
-  pub(crate) const MAX_POSTAGE: Amount = Amount::from_sat(2 * 10_000);
+  pub(crate) const TARGET_POSTAGE: Amount = Amount::from_sat(546);
+  pub(crate) const MAX_POSTAGE: Amount = Amount::from_sat(2 * 546);
 
   pub(crate) fn new(
     outgoing: SatPoint,
